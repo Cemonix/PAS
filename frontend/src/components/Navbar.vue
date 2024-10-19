730127a744d817910d8fe727d53058413b344014
Milan Gittler <gittlermilan@seznam.cz>
2024-10-19 18:54:13 +0200

    feat(frontend-components): Navbar

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+    <nav class="navbar">
+        <ul class="navbar-list">
+            <li>
+                <router-link to="/login" class="navbar-link">Login</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "Navbar",
+});
+</script>
+
+<style scoped>
+    .navbar {
+        background-color: #333;
+        padding: 1rem;
+        display: flex;
+        justify-content: right;
+        flex-shrink: 0;
+    }
+
+    .navbar-list {
+        list-style-type: none;
+        display: flex;
+    }
+
+    .navbar-list li {
+        margin-right: 1.5rem;
+    }
+
+    .navbar-link {
+        text-decoration: none;
+        color: white;
+        font-size: 1rem;
+        transition: color 0.3s ease;
+    }
+
+    .navbar-link:hover {
+        color: #ff6347;
+    }
+</style>
