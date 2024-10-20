3f2523ba387d311164220ddffed2c1fb1d5f36e4
Milan Gittler <gittlermilan@seznam.cz>
2024-10-20 16:15:14 +0200

    refactor(fe-router): Rename router to index

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '../pages/Home.vue';
 import LoginPage from '../pages/Login.vue';
+import DoctorRegistrationPage from '../pages/DoctorRegistration.vue';
 
 const routes = [
+  { path: '/', redirect: '/home' },
+  { path: '/home', component: HomePage },
   { path: '/login', component: LoginPage },
+  { path: '/register-doctor', component: DoctorRegistrationPage },
 ];
 
 const router = createRouter({
