c6c5bc248d00c8209dfb487448916a55b7333f4c
Milan Gittler <gittlermilan@seznam.cz>
2024-10-20 16:18:48 +0200

    feat(fe-component): Navbar role check

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -4,44 +4,58 @@
             <li>
                 <router-link to="/login" class="navbar-link">Login</router-link>
             </li>
+            <li v-if="isAdmin">
+                <router-link to="/register-doctor" class="navbar-link"
+                    >Register Doctor</router-link
+                >
+            </li>
         </ul>
     </nav>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useStore } from 'vuex';
+
+import { key } from '../store';
 
 export default defineComponent({
     name: "Navbar",
+    setup() {
+        const store = useStore(key);
+
+        const isAdmin = store.getters.isAdmin;
+
+        return {
+            isAdmin,
+        };
+    },
 });
 </script>
 
 <style scoped>
-    .navbar {
-        background-color: #333;
-        padding: 1rem;
-        display: flex;
-        justify-content: right;
-        flex-shrink: 0;
-    }
+.navbar {
+    background-color: #333;
+    padding: 1rem;
+    display: flex;
+    justify-content: right;
+    flex-shrink: 0;
+}
 
-    .navbar-list {
-        list-style-type: none;
-        display: flex;
-    }
+.navbar-list {
+    list-style-type: none;
+    display: flex;
+}
 
-    .navbar-list li {
-        margin-right: 1.5rem;
-    }
+.navbar-link {
+    text-decoration: none;
+    color: white;
+    font-size: 1rem;
+    transition: color 0.3s ease;
+    margin-right: 1rem;
+}
 
-    .navbar-link {
-        text-decoration: none;
-        color: white;
-        font-size: 1rem;
-        transition: color 0.3s ease;
-    }
-
-    .navbar-link:hover {
-        color: #ff6347;
-    }
+.navbar-link:hover {
+    color: #ff6347;
+}
 </style>
