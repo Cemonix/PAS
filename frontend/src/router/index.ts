8b4667e264d01f2d79e32202c0b5bfd8994ef5b3
Milan Gittler <gittlermilan@seznam.cz>
2024-11-20 20:34:24 +0100

    feat(frontend-router): Check if authorized before each route

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,18 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { store } from '../store'
 import HomePage from '../pages/Home.vue';
 import LoginPage from '../pages/Login.vue';
 import DoctorRegistrationPage from '../pages/DoctorRegistration.vue';
 
 const routes = [
-  { path: '/', redirect: '/home' },
-  { path: '/home', component: HomePage },
-  { path: '/login', component: LoginPage },
-  { path: '/register-doctor', component: DoctorRegistrationPage },
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: HomePage },
+    { path: '/login', component: LoginPage },
+    {
+        path: '/register-doctor',
+        component: DoctorRegistrationPage,
+        meta: {
+            requiresAuth: true,
+            requiresAdmin: true
+        }
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
+});
+
+router.beforeEach((to, _, next) => {
+    if (to.meta.requiresAuth) {
+        if (!store.state.auth.isAuthenticated) {
+            next('/login');
+            return;
+        }
+
+        if (to.meta.requiresAdmin && !store.getters.isAdmin) {
+            next('/unauthorized');
+            return;
+        }
+    }
+
+    next();
 });
 
 export default router;
