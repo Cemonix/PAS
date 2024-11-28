77f1d85e822d1d795f1fdca974592ee4af6d2e44
Milan Gittler <gittlermilan@seznam.cz>
2024-11-28 20:20:43 +0100

    fixup! feat(server): User routes for profile, password change and account deletion

---
+++ server/src/types/dtos/user/ProfileDto.ts
@@ -0,0 +1,25 @@
+import { Role } from "@prisma/client";
+
+interface BaseProfileDto {
+    email: string;
+    role: Role;
+}
+
+export interface DoctorProfileDto extends BaseProfileDto {
+    firstName: string;
+    lastName: string;
+    contactEmail: string;
+    location: string;
+    phoneNumber: string;
+    specialization: string;
+}
+
+export interface PatientProfileDto extends BaseProfileDto {
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    dateOfBirth: Date;
+    street: string;
+    city: string;
+    postalCode: string;
+}
