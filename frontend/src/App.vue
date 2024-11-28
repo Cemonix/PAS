8522f1709392693076138bd6c03897c640efa683
Milan Gittler <gittlermilan@seznam.cz>
2024-11-28 20:21:47 +0100

    feat(frontend): Added tab

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,20 +6,20 @@
 </template>
 
 <script setup lang="ts">
-import Navbar from "./components/Navbar.vue";
-import {store} from "./store";
+    import Navbar from "./components/Navbar.vue";
+    import {store} from "./store";
 
-const initializeAuth = () => {
-    const token = localStorage.getItem('token');
-    const user = localStorage.getItem('user');
+    const initializeAuth = () => {
+        const token = localStorage.getItem('token');
+        const user = localStorage.getItem('user');
 
-    if (token && user) {
-        store.dispatch('login', {
-            token,
-            user: JSON.parse(user)
-        });
+        if (token && user) {
+            store.dispatch('login', {
+                token,
+                user: JSON.parse(user)
+            });
+        }
     }
-}
 
-initializeAuth();
+    initializeAuth();
 </script>
--- frontend/src/components/RegistrationForm.vue
+++ frontend/src/components/RegistrationForm.vue
@@ -31,63 +31,63 @@
 </script>
 
 <style scoped>
-.registration-page {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgb(150, 150, 150);
-    flex-grow: 1;
-}
+    .registration-page {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: rgb(150, 150, 150);
+        flex-grow: 1;
+    }
 
-.wrapper {
-    width: 840px;
-    background: transparent;
-    border: 2px solid rgba(255, 255, 255, 0.2);
-    border-radius: 10px;
-    backdrop-filter: blur(20px);
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    color: #fff;
-    margin: 15px;
-    padding: 30px 40px;
-}
+    .wrapper {
+        width: 840px;
+        background: transparent;
+        border: 2px solid rgba(255, 255, 255, 0.2);
+        border-radius: 10px;
+        backdrop-filter: blur(20px);
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+        color: #fff;
+        margin: 15px;
+        padding: 30px 40px;
+    }
 
-.wrapper h1 {
-    font-size: 2rem;
-    text-align: center;
-    margin-bottom: 10px;
-}
+    .wrapper h1 {
+        font-size: 2rem;
+        text-align: center;
+        margin-bottom: 10px;
+    }
 
-.wrapper .input-box {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 1rem 1rem;
-    margin: 20px 0;
-    border-radius: 30px;
-    padding: 5px;
-}
+    .wrapper .input-box {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 1rem 1rem;
+        margin: 20px 0;
+        border-radius: 30px;
+        padding: 5px;
+    }
 
-.wrapper .btn {
-    width: 100%;
-    height: 45px;
-    background: #fff;
-    border: none;
-    outline: none;
-    border-radius: 40px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    cursor: pointer;
-    font-size: 1rem;
-    font-weight: 600;
-}
+    .wrapper .btn {
+        width: 100%;
+        height: 45px;
+        background: #fff;
+        border: none;
+        outline: none;
+        border-radius: 40px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        cursor: pointer;
+        font-size: 1rem;
+        font-weight: 600;
+    }
 
-.success-message {
-    text-align: center;
-    color: #4BB543;
-    margin: 10px 0;
-}
+    .success-message {
+        text-align: center;
+        color: #4BB543;
+        margin: 10px 0;
+    }
 
-.error-message {
-    text-align: center;
-    color: rgba(205, 1, 1, 0.829);
-    margin: 10px 0;
-}
+    .error-message {
+        text-align: center;
+        color: rgba(205, 1, 1, 0.829);
+        margin: 10px 0;
+    }
 </style>
